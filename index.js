b79b1a8d124be7b0f0cc9195e40b6738778f7957
unknown <winton2095@gmail.com>
2016-03-29 22:25:57 -0400

    thing

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+var candidates = ["Hillary Clinton","Ted Cruz","Bernie Sanders","Marco Rubio","Ben Carson","Donald J. Trump","John Kasich","Jeb Bush","Scott Walker","Carly Fiorina","Chris Christie","Rand Paul","Rick Perry","Bobby Jindal","Lindsey Graham","Mike Huckabee","Martin O'Malley","George Pataki","Rick Santorum","Lawrence Lessig","Jim Webb","Lincoln Chafee","Jim Gilmore"]
+
 var money = [188.0,104.2,96.3,84.6,68.0,27.3,27.3,157.6,39.5,26.0,26.0,20.6,16.7,11.0,10.0,8.8,7.1,2.1,2.0,1.2,0.8,0.4,0.2]
+
+/*
+d3.select(".money")
+.selectAll("div")
+.data(candidates)
+.enter().append("div")
+.text(function(d) {return d;});
+*/
 
 d3.select(".money")
 .selectAll("div")
 .data(money)
 .enter().append("div")
-.style("width",function(d) {return d * 3 + "px";})
+.style("width",function(d) {return d * 5 + "px";})
+.text(function(d) {return d;});
+
+d3.select(".money")
+.selectAll("div")
+.data(candidates)
+.append("div")
 .text(function(d) {return d;});
