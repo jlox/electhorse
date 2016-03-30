0d1f1e562a06a43d6a2deede8312d3879f99a727
unknown <winton2095@gmail.com>
2016-03-30 00:23:09 -0400

    woo

--- index.js
+++ index.js
@@ -2,58 +2,19 @@ var candidates = ["Hillary Clinton","Ted Cruz","Bernie Sanders","Marco Rubio","B
 
 var money = [188.0,104.2,96.3,84.6,68.0,27.3,27.3,157.6,39.5,26.0,26.0,20.6,16.7,11.0,10.0,8.8,7.1,2.1,2.0,1.2,0.8,0.4,0.2]
 
+var pac = [57.5,49.6,0.1,34.2,10.1,1.8,6.9,124.1,24.1,14.2,18.0,8.5,15.2,4.5,4.4,4.7,1.1,1.5,0.6,0,0.1,0,0]
+
 var delegates = [1266,463,1038,172,0,752,144,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
+var poll = [54,32,36,0,0,42,22,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
 var party = ["d", "r", "d", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "d", "r", "r", "d", "d", "d", "r"]
-
-/*
-var everything = [
-    {cand:"Hillary Clinton", money:188.0, party: 0}
-    {cand:"Ted Cruz", money:104.2, party:1}
-    {cand:"Bernie Sanders", money:96.3, party:0}
-    {cand:"Marco Rubio", money: 84.6, party:1}
-    {cand:"Ben Carson", money:68.0, party:1}
-    {cand:"Donald J. Trump", money:27.3, party:1}
-    {cand:"John Kasich", money:27.3, party:1}
-    {cand:"Jeb Bush", money:157.6, party:1}
-    {cand:"Scott Walker", money:39.5, party:1}
-    {cand:"Carly Fiorina", money:26.0, party:1}
-    {cand:"Chris Christie", money:26.0, party:1}
-    {cand:"Rand Paul", money: 20.6, party:1}
-    {cand:"Rick Perry", money:16.7, party:1}
-    {cand:"Bobby Jindal", money:11.0, party:1}
-    {cand:"Lindsey Graham", money:10.0, party:1}
-    {cand:"Mike Huckabee", money:8.8, party:1}
-    {cand:"Martin O'Malley", money:7.1, party: 0}
-    {cand:"George Pataki", money:2.1, party:1}
-    {cand:"Rick Santorum", money:2.0, party:1}
-    {cand:"Lawrence Lessig", money:1.2, party: 0}
-    {cand:"Jim Webb", money:0.8, party: 0}
-    {cand:"Lincoln Chafee", money:0.4, party: 0}
-    {cand:"Jim Gilmore" money:0.2, party:1}
-]
-*/
-
-/*
-d3.select(".money")
-.selectAll("div")
-.data(candidates)
-.enter().append("div")
-.text(function(d) {return d;});
-*/
 
 d3.select(".money")
 .selectAll("div")
 .data(money)
 .enter().append("div")
 .style("width",function(d) {return d * 5 + "px";})
-/*.style("background-color", function(d){
-    if(d == "d"){
-	return "steelblue";
-    } else {
-	return "#cd5c5c";
-    }
-}*/
 .text(function(d) {return d;});
 
 d3.select(".money")
@@ -84,14 +45,13 @@ d3.select(".d")
 
 d3.select(".m")
 .on("click",function() {
-	d3.select(".money")
-	.selectAll("div")
-	.data(money)
-	.style("width",function(d) {return d * 5 + "px";})
+	d3.select(".money").html("");
 
 	d3.select(".money")
 	.selectAll("div")
 	.data(money)
+	.enter().append("div")
+	.style("width",function(d) {return d * 5 + "px";})
 	.text(function(d) {return d;});
 
 	d3.select(".money")
@@ -102,16 +62,40 @@ d3.select(".m")
 
 });
 
-/*
-d3.select(".money")
-.selectAll("div")
-.data(party)
-.append("div")
-.style("background-color", function(d){
-    if(d=="d"){
-	return "steelblue";
-    } else{
-	return "#cd5c5c";
-    }
-}
-*/
+d3.select(".p")
+.on("click",function() {
+	d3.select(".money").html("");
+
+	d3.select(".money")
+	.selectAll("div")
+	.data(pac)
+	.enter().append("div")
+	.style("width",function(d) {return d * 5 + "px";})
+	.text(function(d) {return d;});
+
+	d3.select(".money")
+	.selectAll("div")
+	.data(candidates)
+	.append("div")
+	.text(function(d) {return d;});
+
+});
+
+d3.select(".s")
+.on("click",function() {
+	d3.select(".money").html("");
+
+	d3.select(".money")
+	.selectAll("div")
+	.data(poll)
+	.enter().append("div")
+	.style("width",function(d) {return d * 10 + "px";})
+	.text(function(d) {return d;});
+
+	d3.select(".money")
+	.selectAll("div")
+	.data(candidates)
+	.append("div")
+	.text(function(d) {return d;});
+
+});
